5e28d982fe774a1332541cd93e73b3ff6a6776fc
Dinuka De Silva <L.dinukadesilva@gmail.com>
2026-02-24 13:41:38 -0500

    Refactoring and refining the styles

--- build.sh
+++ build.sh
@@ -3,13 +3,8 @@
 sass ./b5_ac_conect/scss/style.scss ./b5_ac_conect/css/style.css
 postcss ./b5_ac_conect/css/style.css --replace --use autoprefixer
 
-
 rm -rf ../../drupal-portals/opsportal/web/themes/contrib/b5_ac_conect
-
-
 cp -r ./b5_ac_conect ../../drupal-portals/opsportal/web/themes/contrib/b5_ac_conect
-
-
 cd ../../drupal-portals/opsportal
 ddev drush cr
 cd ../../repo/Operations_Drupal_Theme
